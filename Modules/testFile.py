7a4d4a86d1b57ae86c2049e562aed3b2a19b2590
mcalle1 <mcalle1@sheffield.ac.uk>
2020-04-23 13:39:39 +0100

    Tested module importing and things

---
+++ Modules/testFile.py
@@ -0,0 +1,4 @@
+#testFile.py - For testing the importation of custom modules
+
+def testFunc():
+    print("Test Function Running")
--- PhysAnalysis.py
+++ PhysAnalysis.py
@@ -1,5 +1,7 @@
 #!python3
 #PhysAnalysis.py: A Physics analysis module.
 #Created by: Max Calle
+from Modules import testFile as tf
 
 print("Hello World")
+tf.testFunc()
