b971073f3a128e12b22f1a3967b7cb75837479ef
mcalle1 <mcalle1@sheffield.ac.uk>
2020-04-28 17:48:08 +0100

    Restructured and prepped for PIP

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+'''
+setup.py: Used to configure settings if the package is downloaded from pyip
+'''
+
+setup(
+    name='PhysPack',
+    version='0.0.1',
+    description='A bespoke physics based Analysis Package',
+    license='GNU',
+    packages=['PhysPack'],
+    author='SP-Projects',
+    author_email='macalle@protonmail.com',
+    keywords=['Physics', 'Analysis', 'Graphing', 'Science'],
+    url='https://github.com/Shot-Peen/PhysPack'
+)
